10ae4187f89a65b36cff0bdbec0cbf746d019b9b
Marian Neagul <marian@info.uvt.ro>
2019-11-04 21:23:15 +0200

    cleanup

--- tests/test_basic_flow.py
+++ tests/test_basic_flow.py
@@ -1,15 +1,17 @@
+import os
 from tempfile import NamedTemporaryFile
 
-from hugin.io.loader import BinaryCategoricalConverter
+import pytest
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 
 from hugin.infer.core import KerasPredictor
-
 from hugin.infer.scene import RasterSceneTrainer
+from hugin.io.loader import BinaryCategoricalConverter
+from tests import runningInCI
 
-import os
 
+@pytest.mark.skipif(not runningInCI(), reason="Skipping running locally as it might be too slow")
 def test_keras_train_complete_flow(generated_filesystem_loader):
     mapping = {
         'inputs': {
@@ -41,16 +43,16 @@ def test_keras_train_complete_flow(generated_filesystem_loader):
             name='test_keras_trainer',
             model_path=named_temporary_file.name,
             model_builder="hugin.models.unet.unetv14:unet_v14",
-             batch_size=10,
-             epochs=1,
-             metrics=[
-                 "accuracy"
-             ],
-             loss="categorical_crossentropy",
-             optimizer=Adam(),
-             callbacks=[
-                 EarlyStopping(monitor='loss')
-             ]
+            batch_size=10,
+            epochs=1,
+            metrics=[
+                "accuracy"
+            ],
+            loss="categorical_crossentropy",
+            optimizer=Adam(),
+            callbacks=[
+                EarlyStopping(monitor='loss')
+            ]
         )
         trainer = RasterSceneTrainer(name="test_raster_trainer",
                                      stride_size=256,
